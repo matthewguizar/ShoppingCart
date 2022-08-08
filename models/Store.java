0a322d0e506ddad918fb7c40fc737beacd0901b0
Matthew Guizar <matthewrguizar@gmail.com>
2022-08-08 10:42:51 -0700

    tested adding items into store and created toString for store

--- models/Store.java
+++ models/Store.java
@@ -16,4 +16,27 @@ public class Store {
     public void setItems(int row, int column, Item item) {
         this.items[row][column] = new Item(item);
     }
+
+    @Override
+    public String toString() {
+        String temp = "";
+        for (int i = 0; i < items.length; i++) {
+            switch (i) {
+                case 0: temp += "\tDRINKS:        "; break;
+                case 1: temp += "\tCEREAL:        "; break;
+                case 2: temp += "\tDAIRY:         "; break; 
+                case 3: temp += "\tDELI:          "; break; 
+                case 4: temp += "\tGREENS:        "; break; 
+                case 5: temp += "\tCLOTHING:      "; break; 
+                case 6: temp += "\tELECTRONICS:   "; break; 
+            }        
+            for (int j = 0; j < items[i].length; j++) {
+                temp += items[i][j].toString();
+                
+            }
+            temp += "\n\n";
+        }
+        temp +="\t************************************************************************\n";
+        return temp;
+    }
 }
